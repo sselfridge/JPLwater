803f9f1d8cb05b784027cdb73557d7bd8b26fb25
Sam Selfridge <Sam.Selfridge@gmail.com>
2020-08-23 17:33:38 -0700

    both work?

--- server/controllers/piController.js
+++ server/controllers/piController.js
@@ -55,7 +55,7 @@ if (CURRENT_ENV === 'production') {
     const Gpio = onoff.Gpio;
     piController.objIO.motion = new Gpio(4, 'in');
     piController.objIO.door = new Gpio(23, 'in');
-    piController.objIO.green =  new Gpio(17, 'low');
+    piController.objIO.green =  new Gpio(17, 'both');
 
   } else {
     piController.objIO.motion = new MockGpio(0);
